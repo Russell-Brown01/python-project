3b84e21824d8490bad218a792504b300dc9367ca
Russell-Brown01 <71991847+Russell-Brown01@users.noreply.github.com>
2020-09-27 20:44:02 -0700

    9/27/20 20:43

--- Main.py
+++ Main.py
@@ -8,21 +8,41 @@ print(dir_list)
 print()
 game = input()
 print(game)
+enterData = ''
 
 def datafile(path, file):
     print("File not found, making one.")
     with open(os.path.join(path, file), 'w+') as fp:
         pass
     return 1
+def matchStats():
+    while enterData == '1':
+        fo = open('ApexStats.txt', 'a')
+        gameNum = ''
+        gameDate = ''
+        gameKills = ''
+        print('Match Number?')
+        input(gameNum)
+        print('Match Kills?')
+        input(gameKills)
+        print('Match Date?')
+        input(gameDate)
+        fo.write(gameNum, gameDate, gameKills)
 #Check for Apex data file and create one if not found.
 if game == '1':
     print("Opening Apex data...")
     file = 'ApexStats.txt'
     path = 'C:\\Users\\rtb32\\Desktop\\StatTrackerPY\\APEX_Stats'
+    enterData = ''
     if os.path.isfile('C:\\Users\\rtb32\\Desktop\\StatTrackerPY\\APEX_Stats\\ApexStats.txt'): #Can you call function from inside a function?
         print("Stat file found! Enter new data?")
+        print('Yes/No')
+        input(enterData)
+        print(enterData)
+        matchStats()
     else:
-        datafile('C:\\Users\\rtb32\\Desktop\\StatTrackerPY\\APEX_Stats', 'ApexStats.txt')
+        print("File Not Found.")
+
 if game == '2':
     print("Opening CSGO data...")
     path = 'C:\\Users\\rtb32\\Desktop\\StatTrackerPY\\CSGO_Stats'
